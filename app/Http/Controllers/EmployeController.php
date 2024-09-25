900231ae8ad58e9101cd77644db5fd01ba9bacc8
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-09-25 14:27:45 +0200

    Ajout des méthodes pour l'affichage, la création et la suppression d'un employé

--- app/Http/Controllers/EmployeController.php
+++ app/Http/Controllers/EmployeController.php
@@ -3,64 +3,74 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employe;
+use App\Models\Departement;
 use App\Http\Requests\StoreEmployeRequest;
 use App\Http\Requests\UpdateEmployeRequest;
 
 class EmployeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $employes = Employe::latest()->get();
+
+        return view("employes.index", compact("employes"));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        $departements = Departement::latest()->get();
+
+        return view('employes.create', compact('departements'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreEmployeRequest $request)
     {
-        //
+        try {
+            $employe = Employe::firstOrCreate([
+                'nom' => $request->input('nom'),
+                'postnom' => $request->input('postnom'),
+                'prenom' => $request->input('prenom'),
+                'genre' => $request->input('genre'),
+                'etat_civil' => $request->input('etat_civil'),
+                'nationalite' => $request->input('nationalite'),
+                'date_naissance' => $request->input('date_naissance'),
+                'adresse_physique' => $request->input('adresse_physique'),
+                'telephone' => $request->input('telephone'),
+                'date_embauche' => $request->input('date_embauche'),
+                'salaire' => $request->input('salaire'),
+                'poste' => $request->input('poste'),
+                'departement_id' => $request->input('departement_id'),
+            ]);
+            return redirect()->route('employes.index')->with('success', 'Employé enregistré avec succès!');
+        } catch (\Throwable $th) {
+            return back()->withErrors(['error', 'Une erreur s\'est produite lors de la creation du nouvel employé.'])->withInput();
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Employe $employe)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Employe $employe)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateEmployeRequest $request, Employe $employe)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Employe $employe)
     {
-        //
+        try {
+            $employe->delete();
+
+            return redirect()->route('employes.index')->with('success', 'Employé supprimé avec succès !');
+        } catch (\Throwable $th) {
+            return back()->with('error', 'Une erreur s\'est produite lors de la suppression de l\'employé !');
+        }
     }
 }
--- app/Http/Requests/StoreEmployeRequest.php
+++ app/Http/Requests/StoreEmployeRequest.php
@@ -6,23 +6,50 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmployeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'nom' => 'required|string|max:255',
+            'postnom' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'genre' => 'required|in:Homme,Femme',
+            'etat_civil' => 'required|string',
+            'nationalite' => 'required|string|max:255',
+            'date_naissance' => 'required|date',
+            'adresse_physique' => 'required|string',
+            'telephone' => 'required|string|max:20',
+            'date_embauche' => 'required|date',
+            'salaire' => 'required|numeric',
+            'poste' => 'required|string|max:255',
+            'departement_id' => 'required|exists:departements,id',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'nom.required' => 'Le nom est requis.',
+            'postnom.required' => 'Le postnom est requis.',
+            'prenom.required' => 'Le prénom est requis.',
+            'genre.required' => 'Le genre est requis.',
+            'genre.in' => 'Le genre doit être soit "homme" soit "femme".',
+            'etat_civil.required' => "L'état civil est requis.",
+            'nationalite.required' => 'La nationalité est requise.',
+            'date_naissance.required' => 'La date de naissance est requise.',
+            'adresse_physique.required' => "L'adresse physique est requise.",
+            'telephone.required' => 'Le numéro de téléphone est requis.',
+            'date_embauche.required' => "La date d'embauche est requise.",
+            'salaire.required' => 'Le salaire est requis.',
+            'salaire.numeric' => 'Le salaire doit être un nombre.',
+            'poste.required' => 'Le poste est requis.',
+            'departement_id.required' => 'Le département est requis.',
+            'departement_id.exists' => 'Le département sélectionné est invalide.',
+        ];
+    }
+
 }
