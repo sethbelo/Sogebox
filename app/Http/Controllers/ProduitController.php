33f5de6c1ca6c2b0f1c5fc8d700979e0ebcbf27b
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-05 15:23:40 +0200

    Ajout de la fonctionnnalité de recherche pour les produits

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -3,62 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produit;
+use Illuminate\Http\Request;
 use App\Http\Requests\StoreProduitRequest;
 use App\Http\Requests\UpdateProduitRequest;
 
 class ProduitController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    public function search(Request $request)
+    {
+        $searchTerm = $request->input('search');
+        $produits = Produit::where('produits', 'LIKE', "%{$searchTerm}%")
+            ->take(6)
+            ->latest()
+            ->get();
+            
+        return response()->json($produits);
+    }
+
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreProduitRequest $request)
     {
         //
     }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(Produit $produit)
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Produit $produit)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(UpdateProduitRequest $request, Produit $produit)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Produit $produit)
     {
         //
