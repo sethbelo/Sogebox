fe3ae8a9f904e237e6e9758438e537c9f07d0f7a
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-10-05 15:26:00 +0200

    Génération d'un statut aléatoire (fake) pour les commandes

--- database/factories/CommandeFactory.php
+++ database/factories/CommandeFactory.php
@@ -10,9 +10,8 @@ class CommandeFactory extends Factory
     public function definition(): array
     {
         return [
-            'numero_commande' => Commande::generateOrderNumber(),
             'date_commande' => $this->faker->date(),
-            'statut' => $this->faker->randomElement(['livrée', 'annulée', 'en attente']), // Statut aléatoire
+            'statut' => $this->faker->randomElement(['Livrée', 'Annulée', 'En attente']), // Statut aléatoire
             'frais_main_oeuvre' => $this->faker->numberBetween(1, 1000),
             'frais_livraison' => $this->faker->numberBetween(1, 1000),
             'client_id' => \App\Models\Client::factory(), // Utilisation de la factory de Client
